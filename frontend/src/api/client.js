d34e453eb556b992adad2edaf049f28ada052190
DSanish <anishkumar.tech26@gmail.com>
2026-08-02 10:18:09 +0530

    .env add

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base URL from .env
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api/v1";
+  import.meta.env.VITE_API_BASE_URL || "/api/v1";
 
 // Create Axios Instance
 const apiClient = axios.create({
